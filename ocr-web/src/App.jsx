02cc1a41385db72ce560901aee122149fa4935d0
DUDADIDO <starfbsdud@gmail.com>
2025-06-16 19:06:46 +0900

    update
    업데이트

---
+++ ocr-web/src/App.jsx
@@ -0,0 +1,67 @@
+// src/App.js
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+
+import UploadPage  from './pages/UploadPage.jsx';
+import ResultsPage from './pages/ResultsPage.jsx';
+import FailedPage  from './pages/FailedPage.jsx';
+
+import './App.css';
+
+export default function App() {
+  // 공통 클래스
+  const baseLink = "px-2 py-1 transition-colors duration-200";
+  
+  return (
+    <Router>
+      <nav className="nav-links flex justify-center items-center space-x-6 border-b-2 bg-white">
+        <NavLink
+          to="/"
+          end
+          className={({ isActive }) =>
+            `${baseLink} ${
+              isActive
+                ? "text-blue-600 border-b-2 border-blue-600"
+                : "text-black hover:text-blue-600"
+            }`
+          }
+        >
+          메인
+        </NavLink>
+        <NavLink
+          to="/results"
+          className={({ isActive }) =>
+            `${baseLink} ${
+              isActive
+                ? "text-blue-600 border-b-2 border-blue-600"
+                : "text-black hover:text-blue-600"
+            }`
+          }
+        >
+          미리보기
+        </NavLink>
+        <NavLink
+          to="/failed"
+          className={({ isActive }) =>
+            `${baseLink} ${
+              isActive
+                ? "text-blue-600 border-b-2 border-blue-600"
+                : "text-black hover:text-blue-600"
+            }`
+          }
+        >
+          인식실패
+        </NavLink>
+      </nav>
+
+      <div className="page-container">
+        <Routes>
+          <Route path="/" element={<UploadPage />} />
+          <Route path="/results" element={<ResultsPage />} />
+          <Route path="/failed" element={<FailedPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
---
+++ ocr-web/src/components/Hello.jsx
@@ -0,0 +1,6 @@
+// src/components/Hello.js
+import React from 'react';
+
+export default function Hello({ name }) {
+  return <h2>안녕하세요, {name}님!</h2>;
+}
